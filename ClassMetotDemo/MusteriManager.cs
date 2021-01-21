80159bbe5b1e1612ffe9b4e9b3dc43e801356e79
beko5 <beko5@DESKTOP-GUEE677>
2021-01-21 23:09:03 +0300

    update

--- ClassMetotDemo/MusteriManager.cs
+++ ClassMetotDemo/MusteriManager.cs
@@ -8,11 +8,11 @@ namespace ClassMetotDemo
     {
         public void Ekle(Musteri musteri)
         {
-            Console.WriteLine("Müşteri Eklendi : " + musteri.Id + " " + musteri.Name + " " + musteri.Surname + " " + musteri.CardNo);
+            Console.WriteLine("Müşteri Eklendi : {0} {1} {2} {3} ", musteri.Id, musteri.Name, musteri.Surname, musteri.CardNo);
         }
         public void Sil(Musteri musteri)
         {
-            Console.WriteLine("Müşteri silindi : " + musteri.Id + " " + musteri.Name + " " + musteri.Surname + " " + musteri.CardNo);
+            Console.WriteLine("Müşteri silindi : {0} {1} {2} {3} ", musteri.Id, musteri.Name, musteri.Surname, musteri.CardNo);
         }
 
 
